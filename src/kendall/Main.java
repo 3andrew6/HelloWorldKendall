339374ec78f612df65b7e40844545d83f4b130a8
Jermery Wheeler <52702506+jermery614@users.noreply.github.com>
2020-10-11 14:15:41 -0400

    Added to the file.

--- src/kendall/Main.java
+++ src/kendall/Main.java
@@ -3,6 +3,8 @@ package kendall;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, my name is Andrew Kendall");
+        System.out.println("Hello, my name is Jermery Wheeler");
     }
 }
